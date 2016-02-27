c13e3c1391d8de33d1d849d3967e6def1c05f307
Sunjot Singh <sunjot@uoguelph.ca>
2016-02-27 02:06:32 -0500

    added mobile code

--- assets/js/animate.js
+++ assets/js/animate.js
@@ -1,39 +1,42 @@
 $(document).ready(function() {
 
-    // If mouse hovers over a shield in row 1, display info
-    $('#row1 .flag').hover(function() {
-      var temp = '#row1box ' + '#' + $(this).attr('id');
-      $(temp).css('display', 'block');
-    }, function() {
-      var temp = '#row1box ' + '#' + $(this).attr('id');
-      $(temp).css('display', 'none');
-    });
+    if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) {
+        $('.fade').css('opacity', '100');
+    } else {
 
-    // If mouse hovers over a sheild in row 2, display info
-    $('#row2 .flag').hover(function() {
-      var temp = '#row2box ' + '#' + $(this).attr('id');
-      $(temp).css('display', 'block');
-    }, function() {
-      var temp = '#row2box ' + '#' + $(this).attr('id');
-      $(temp).css('display', 'none');
-    });
+      // If mouse hovers over a shield in row 1, display info
+      $('#row1 .flag').hover(function() {
+        var temp = '#row1box ' + '#' + $(this).attr('id');
+        $(temp).css('display', 'block');
+      }, function() {
+        var temp = '#row1box ' + '#' + $(this).attr('id');
+        $(temp).css('display', 'none');
+      });
 
-    /* Every time the window is scrolled ... */
-    $(window).scroll( function(){
+      // If mouse hovers over a sheild in row 2, display info
+      $('#row2 .flag').hover(function() {
+        var temp = '#row2box ' + '#' + $(this).attr('id');
+        $(temp).css('display', 'block');
+      }, function() {
+        var temp = '#row2box ' + '#' + $(this).attr('id');
+        $(temp).css('display', 'none');
+      });
 
-        /* Check the location of each desired element */
-        $('.fade').each( function(i){
+      /* Every time the window is scrolled ... */
+      $(window).scroll( function(){
 
-          var h = window.innerHeight / 2;
+          /* Check the location of each desired element */
+          $('.fade').each( function(i){
 
-          if (($(this).offset().top - $(window).scrollTop()) < h) {
-            $(this).stop().fadeTo(100, 1);
-          } else {
-            $(this).stop().fadeTo(100, 0);
-          }
+            var h = window.innerHeight / 2;
 
-        });
+            if (($(this).offset().top - $(window).scrollTop()) < h) {
+              $(this).stop().fadeTo(100, 1);
+            } else {
+              $(this).stop().fadeTo(100, 0);
+            }
 
-    });
-
+          });
+      });
+    }
 });
